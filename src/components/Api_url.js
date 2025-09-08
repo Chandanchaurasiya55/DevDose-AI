c563631d27e00e77ce477e2f1902220b9d278e15
Chandan kumar <120443395+Chandan-kumar5@users.noreply.github.com>
2025-09-08 17:13:47 +0530

    First commit with React project

--- src/components/Api_url.js
+++ src/components/Api_url.js
@@ -3,6 +3,7 @@
 export const URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${import.meta.env.VITE_API_KEY}`;
 
 
+console.log("API KEY from env 👉", import.meta.env.VITE_API_KEY);
 
 
 
